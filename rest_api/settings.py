cedf4e6f3c26df86411b7bf8c1e395b93125987c
pontat <ponta.t.dev@gmail.com>
2021-09-26 21:53:17 +0900

    feat:added cors white list

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "http://localhost:3000", "https://next-blog-todos.vercel.app"
 ]
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
